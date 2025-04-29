da839ea169eca4ab53bf0cd61eb544945e46ad35
jukka211 <158295066+jukka211@users.noreply.github.com>
2025-04-29 13:41:15 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,9 +14,10 @@ function setup() {
   mic = new p5.AudioIn();
   mic.start();
 
-  colors = ['red', 'limegreen', 'blue'];
+  colors = ['black', 'white', 'red', 'limegreen', 'blue'];
   currentColor = color(random(colors));
   targetColor = currentColor;
+  noStroke();
 }
 
 function draw() {
@@ -47,7 +48,12 @@ function draw() {
 
   background(currentColor);
 
-  fill(0);
+  // Set fill color based on background
+  if (currentColor.toString() === color('black').toString()) {
+    fill(255); // white
+  } else {
+    fill(0); // black
+  }
 
   // Horizontal blocks (static)
   rect(31, 354, 23, 23);   
@@ -57,10 +63,10 @@ function draw() {
 
   // Vertical blocks (animated)
   rect(31, 396, 23, map(level * 8.0, 0, 1, 75, 300));   
-  rect(90, 295, 23, map(level * 12, 0, 1, 220, 400));  
-  rect(134, 340, 23, map(level * 6.5, 0, 1, 210, 400));
+  rect(90, 295, 23, map(level * 15, 0, 1, 220, 400));  
+  rect(134, 340, 23, map(level * 7.5, 0, 1, 210, 400));
   rect(185, 272, 23, map(level * 3.5, 0, 1, 210, 400));
   rect(225, 272, 23, map(level * 8.5, 0, 1, 280, 500));
-  rect(293, 336, 23, map(level * 1.8, 0, 1, 165, 300));
-  rect(336, 336, 23, map(level * 8.2, 0, 1, 134, 300));
+  rect(293, 336, 23, map(level * 4.8, 0, 1, 165, 300));
+  rect(336, 336, 23, map(level * 9.2, 0, 1, 134, 300));
 }
